b94a3243661590d0c44a7c4fe4f0c98dc1f2716d
Nathanviana <viananathan03@gmail.com>
2024-08-03 13:28:45 -0300

    update

--- src/assets/components/Todo.jsx
+++ src/assets/components/Todo.jsx
@@ -7,9 +7,8 @@ const Todo = ({ todo, removeTodo, completeTodo }) => {
       <h2>{todo.text}</h2>
       <div className="category">{todo.isCompleted}</div>
     </div>
-    {/* MEXER NO TAMANHO, FONTE, LIMITE DE CARACTER */}
     <div className="description">
-      <textarea placeholder="Texto"></textarea>
+      <textarea maxLength={170} placeholder="Texto"></textarea>
     </div>
     <div className="mainButtons">
       <button><i class="bi bi-check-square" onClick={() => completeTodo(todo.id)}></i></button>
