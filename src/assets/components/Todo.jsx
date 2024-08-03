225570fae50f64d2eb2de5e18707e10054fd0f78
Pedro Silva <pedro.o.silva975@gmail.com>
2024-08-03 13:36:39 -0300

    update textarea

--- src/assets/components/Todo.jsx
+++ src/assets/components/Todo.jsx
@@ -7,36 +7,15 @@ const Todo = ({ todo, removeTodo, completeTodo }) => {
         <h2 style={{textDecoration: todo.isCompletedValidation ? "line-through" : ""}}>{todo.text}</h2>
         <div className="category">{todo.isCompletedValidation ? "Concluído" : "Pendente"}</div>
       </div>
-      {/* MEXER NO TAMANHO, FONTE, LIMITE DE CARACTER */}
       <div className="description">
-        <textarea placeholder="Texto"></textarea>
+        <textarea maxLength={170} placeholder="Texto"></textarea>
       </div>
       <div className="mainButtons">
-        <button>
-          <i
-            className="bi bi-check-square"
-            onClick={() => completeTodo(todo.id)}
-          ></i>
-        </button>
-        <button className="remove" onClick={() => removeTodo(todo.id)}>
-          <i className="bi bi-trash"></i>
-        </button>
+        <button><i class="bi bi-check-square" onClick={() => completeTodo(todo.id)}></i></button>
+        <button className="remove" onClick={() => removeTodo(todo.id)}><i class="bi bi-trash"></i></button>
       </div>
     </div>
-<<<<<<< HEAD
-    <div className="description">
-      <textarea maxLength={170} placeholder="Texto"></textarea>
-    </div>
-    <div className="mainButtons">
-      <button><i class="bi bi-check-square" onClick={() => completeTodo(todo.id)}></i></button>
-      <button className="remove" onClick={() => removeTodo(todo.id)}><i class="bi bi-trash"></i></button>
-    </div>
-  </div>
   )
 }
-=======
-  );
-};
->>>>>>> 8e68e92bc4ed40a57bdb3cf3fe6335c70a4c2f3d
 
 export default Todo;
